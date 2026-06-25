0ca4b13529e191f82ad6519defc4c709b506ff97
Ari <147622495+Ari1416@users.noreply.github.com>
2026-06-24 22:57:23 -0400

    Revert "Arianna y Máximo: Empresas, sectores y tamaños"

--- src/company-sizes/company-sizes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CompanySizesController } from './company-sizes.controller';
-import { CompanySizesService } from './company-sizes.service';
-
-@Module({
-  controllers: [CompanySizesController],
-  providers: [CompanySizesService],
-})
-export class CompanySizesModule {}
--- src/subsectors/subsectors.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SubsectorsController } from './subsectors.controller';
-import { SubsectorsService } from './subsectors.service';
-
-@Module({
-  controllers: [SubsectorsController],
-  providers: [SubsectorsService],
-})
-export class SubsectorsModule {}
